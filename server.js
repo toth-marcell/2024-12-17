d72295fa8f837eee87221f169dbc317d9b7280d9
toth-marcell <toth.marcell.21i@egressy.info>
2024-12-18 09:41:17 +0100

    rmWhitespace, formatting

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
+app.set("view options", {
+  rmWhitespace: true,
+});
 app.get("/", async (req, res) => {
   res.render("index", { decors: await Decoration.findAll() });
 });
